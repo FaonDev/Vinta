db1ef560721f1266ada7a56330f2f893f40d4f40
FaonDev <124811747+FaonDev@users.noreply.github.com>
2023-04-15 20:49:54 -0300

    Tiny update

--- .vinta/assets/welcome.ts
+++ .vinta/assets/welcome.ts
@@ -3,6 +3,6 @@ import Chalk from 'chalk'
 
 console.clear()
 console.log(`${Chalk.magenta('Vinta.js')} ${Chalk.bgYellow('v1.0.0')}`)
-console.log(Chalk.grey(`Assisted ${Chalk.white.underline('TypeScript')} compilation enabled. ${Chalk.green.italic('Happy hacking!')}`))
+console.log(Chalk.grey(`Watched ${Chalk.white.underline('TypeScript')} compilation enabled. ${Chalk.green.italic('Happy hacking!')}`))
 console.log(`Running on ${Chalk.blue(`http://localhost:${(expressListener.address() as any).port}`)}`)
 console.log(Chalk.dim('...'))
